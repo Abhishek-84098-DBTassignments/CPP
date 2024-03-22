0879ccf8d33c5c20f765fefa05d73dd57450de43
joshiabhishek0392 <joshiabhishek0392@gmail.com>
2024-03-22 14:42:58 +0530

    assignment2q3 uploaded

---
+++ assignment2/Assignment2_3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class address
+{
+    private:
+
+        string building;
+        string street;
+        string city;
+        int pin;
+
+    public:
+
+        
+};
